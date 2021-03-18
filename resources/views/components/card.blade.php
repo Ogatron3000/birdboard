cde2c6808e485dbf52d73da1002a3da953c67c52
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2021-03-18 17:37:25 +0100

    flex 1 instead of fixed height for description

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,10 +1,10 @@
 <div {{ $attributes->merge(['class' => "p-4 pt-0"]) }}>
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="h-52 p-6 bg-white border-b border-gray-200">
+        <div class="flex flex-col h-52 p-6 bg-white border-b border-gray-200">
             <h3 class="py-2 mb-4 -ml-6 border-l-4 border-green-400 pl-5 text-lg">
                 {{ $title }}
             </h3>
-            <p class="break-words h-24">
+            <p class="break-words flex-1">
                 {{ $description }}
             </p>
             <div class="text-right text-xs">
